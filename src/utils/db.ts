00c22cf2098281a3e000f12c76b4e71093f2d13f
srsean <seanlibiran@gmail.com>
2025-01-08 09:44:14 +0800

    refactor: update Prisma client initialization for better singleton management

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,15 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaSingleton = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-declare global {
-  var prismaGlobal: undefined | PrismaClient;
-}
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaSingleton;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
 
-if (process.env.NODE_ENV === "development") {
-  globalThis.prismaGlobal = prisma;
-}
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
