f77c4bf5c9355339724277efc239635db256f1ab
srsean <seanlibiran@gmail.com>
2025-01-08 09:44:14 +0800

    feat: enhance layout structure for login and registration pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -17,32 +17,30 @@ const initialState: UserLoginResponse = {
 
 export default async function Login() {
   return (
-    <section className="bg-white">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8 text-center">
-            <div className="flex justify-center">
-              <Image
-                src="/images/logo.png" // Route of the image file
-                height={60} // Desired size with correct aspect ratio
-                width={60} // Desired size with correct aspect ratio
-                alt="logo"
-              />
-            </div>
-            <h1 className="text-[22px] font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Log in to Undetectable AI
-            </h1>
-            <h3 className="text-[16px] text-[#14151A]">Enter your username and password to continue</h3>
-            <Form initialState={initialState} action={loginUser}>
-              <div>
-                <TextInput name="username" placeholder="Username" />
-              </div>
-              <div>
-                <PasswordInput name="password" />
-              </div>
-              <SubmitButton className="rounded-xl">Log in</SubmitButton>
-            </Form>
+    <section className="bg-white flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+      <div className="w-full bg-white rounded-lg lg:shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8 text-center">
+          <div className="flex justify-center">
+            <Image
+              src="/images/logo.png" // Route of the image file
+              height={60} // Desired size with correct aspect ratio
+              width={60} // Desired size with correct aspect ratio
+              alt="logo"
+            />
           </div>
+          <h1 className="text-[22px] font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            Log in to Undetectable AI
+          </h1>
+          <h3 className="text-[16px] text-[#14151A]">Enter your username and password to continue</h3>
+          <Form initialState={initialState} action={loginUser}>
+            <div>
+              <TextInput name="username" placeholder="Username" />
+            </div>
+            <div>
+              <PasswordInput name="password" />
+            </div>
+            <SubmitButton className="rounded-xl">Log in</SubmitButton>
+          </Form>
         </div>
       </div>
     </section>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -16,45 +16,43 @@ const initialState: UserRegistrationResponse = {
 
 export default async function Register() {
   return (
-    <section className="bg-white">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8 text-center">
-            <div className="flex justify-center">
-              <Image
-                src="/images/logo.png" // Route of the image file
-                height={60} // Desired size with correct aspect ratio
-                width={60} // Desired size with correct aspect ratio
-                alt="logo"
-              />
-            </div>
-            <h1 className="text-[22px] font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Register to Undetectable AI
-            </h1>
-            <h3 className="text-[16px] text-[#14151A]"> Please fill in all fields to create your account</h3>
-
-            <Form initialState={initialState} action={registerUser}>
-              <div>
-                <TextInput name="firstName" placeholder="First Name" />
-              </div>
-              <div>
-                <TextInput name="lastName" placeholder="Last Name" />
-              </div>
-              <div>
-                <TextInput name="email" placeholder="Email" />
-              </div>
-              <div>
-                <TextInput name="username" placeholder="Username" />
-              </div>
-              <div>
-                <PasswordInput name="password" />
-              </div>
-              <div>
-                <PasswordInput name="confirmPassword" placeholder="Confirm Password" />
-              </div>
-              <SubmitButton className="rounded-xl">Create Account</SubmitButton>
-            </Form>
+    <section className="bg-white flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+      <div className="w-full bg-white rounded-lg lg:shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8 text-center">
+          <div className="flex justify-center">
+            <Image
+              src="/images/logo.png" // Route of the image file
+              height={60} // Desired size with correct aspect ratio
+              width={60} // Desired size with correct aspect ratio
+              alt="logo"
+            />
           </div>
+          <h1 className="text-[22px] font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            Register to Undetectable AI
+          </h1>
+          <h3 className="text-[16px] text-[#14151A]"> Please fill in all fields to create your account</h3>
+
+          <Form initialState={initialState} action={registerUser}>
+            <div>
+              <TextInput name="firstName" placeholder="First Name" />
+            </div>
+            <div>
+              <TextInput name="lastName" placeholder="Last Name" />
+            </div>
+            <div>
+              <TextInput name="email" placeholder="Email" />
+            </div>
+            <div>
+              <TextInput name="username" placeholder="Username" />
+            </div>
+            <div>
+              <PasswordInput name="password" />
+            </div>
+            <div>
+              <PasswordInput name="confirmPassword" placeholder="Confirm Password" />
+            </div>
+            <SubmitButton className="rounded-xl">Create Account</SubmitButton>
+          </Form>
         </div>
       </div>
     </section>
